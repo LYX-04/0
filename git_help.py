0a6eef13286719339aee4700bcb458e2bd0bd040
LYX-04 <cry.0409@qq.com>
2024-08-08 10:32:13 +0800

    Update git_help.py

--- git_help.py
+++ git_help.py
@@ -7,3 +7,21 @@ git init
 
 #查看
 git status
+
+#将文件提交至暂存区
+git add 文件名
+
+#将暂存区的文件提交至本地库
+git commit -m "日志信息" 文件名
+
+#给远程仓库起别名
+git remote add 别名 远程地址
+
+#查看别名
+git remote -v
+
+#将本地文件上传至Github
+git push 别名 分支
+
+#从远端仓库下载文件
+git clone 远程地址
